2a850914468588603ac92fafec6b5bda440ea4bf
RanggaAlroy <114912822+RanggaAlroy@users.noreply.github.com>
2023-10-08 10:20:54 +0700

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Hello welcome to Next Js 13</h1>
-      <h1 className="h2-bold">Hello welcome to Next Js 13</h1>
-      <h1 className="h3-bold">Hello welcome to Next Js 13</h1>
-    </div>
-  );
-}
