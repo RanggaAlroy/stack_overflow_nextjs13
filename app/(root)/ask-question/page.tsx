21f4ce533b609aa6692352621c2a033d5fab6b4c
RanggaAlroy <114912822+RanggaAlroy@users.noreply.github.com>
2023-10-19 10:40:59 +0700

    imported form and TinyMCE Editor

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
+import Question from '@/components/forms/Question';
 
-const page = () => {
-  return <div>Ask Question</div>;
+const Page = () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
